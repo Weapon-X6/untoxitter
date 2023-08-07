4116fbd640883ced7fd5d9d1e243645714f66ab9
waffe_x20 <argueta_eduardo@yahoo.de>
2023-08-06 23:06:00 -0600

    Add signal to create profiles

--- trots/models.py
+++ trots/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 class Profile(models.Model):
@@ -11,3 +11,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+
+
+# Create a Profile for each new user
+post_save.connect(create_profile, User)
